d49aeb397ec185072a13952ba168a9e103f2e580
bugPirates <ican.notfound@gmail.com>
2022-06-06 14:03:37 +0800

    create post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'articles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
